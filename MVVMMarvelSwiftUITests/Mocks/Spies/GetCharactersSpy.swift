5c63d6475df26893337fb62bc4ef8e7cf26a7e16
Nacho <nacho@Nachos-MacBook-Pro.local>
2024-03-23 18:37:35 -0300

    ViewModel tests

---
+++ MVVMMarvelSwiftUITests/Mocks/Spies/GetCharactersSpy.swift
@@ -0,0 +1,42 @@
+//
+//  GetCharactersSpy.swift
+//  MVVMMarvelSwiftUITests
+//
+//  Created by Nacho on 23/03/2024.
+//
+
+import Foundation
+@testable import MVVMMarvelSwiftUI
+
+class GetCharactersSpy: GetCharacters {
+    var invokedExecuteSuccess = false
+    var invokedExecuteSuccessCount = 0
+    var stubbedExecuteSuccessSuccessResult: ([CharacterModel], Void)?
+    var stubbedExecuteSuccessFailureResult: (GetCharactersError, Void)?
+
+    func execute(success: @escaping ([CharacterModel]) -> Void,
+        failure: @escaping (GetCharactersError) -> Void) {
+        invokedExecuteSuccess = true
+        invokedExecuteSuccessCount += 1
+        if let result = stubbedExecuteSuccessSuccessResult {
+            success(result.0)
+        }
+        if let result = stubbedExecuteSuccessFailureResult {
+            failure(result.0)
+        }
+    }
+
+    var invokedExecute = false
+    var invokedExecuteCount = 0
+    var stubbedExecuteSuccessResult: [CharacterModel]!
+    var stubbedExecuteFailureResult: NSError?
+
+    func execute() async throws -> [MVVMMarvelSwiftUI.CharacterModel] {
+        invokedExecute = true
+        invokedExecuteCount += 1
+        if let stubbedExecuteFailureResult = stubbedExecuteFailureResult {
+            throw stubbedExecuteFailureResult
+        }
+        return stubbedExecuteSuccessResult
+    }
+}
---
+++ MVVMMarvelSwiftUITests/Presentation/CharacterList/ViewModel/CharacterListViewModelTests.swift
@@ -0,0 +1,51 @@
+//
+//  CharacterListViewModelTests.swift
+//  MVVMMarvelSwiftUITests
+//
+//  Created by Nacho on 23/03/2024.
+//
+
+import XCTest
+@testable import MVVMMarvelSwiftUI
+
+
+class CharacterListViewModelTests: XCTestCase {
+    private var sut: CharacterListViewModel!
+    private var getCharacterSpy: GetCharactersSpy!
+
+    func testLoadCharactersAwait() async {
+        givenASUT()
+        await whenLoadCharactersAwaitIsExecuted()
+        await thenAssertLoadCharactersAwaitLoad()
+    }
+
+    private func givenASUT() {
+        getCharacterSpy = GetCharactersSpy()
+        sut = CharacterListViewModel(getCharacters: getCharacterSpy)
+    }
+
+    private func whenLoadCharactersAwaitIsExecuted() async {
+        let firstCharacter = CharacterModel(id: 1,
+                                            name: "Bronco",
+                                            description: "The best Marvel character",
+                                            thumbnail: Thumbnail(path: "",
+                                                                 imageExtension: ""))
+        let secondCharacter = CharacterModel(id: 2,
+                                             name: "Spiderman",
+                                             description: "The second best Marvel character",
+                                             thumbnail: Thumbnail(path: "",
+                                                                  imageExtension: ""))
+        getCharacterSpy.stubbedExecuteSuccessResult = [firstCharacter, secondCharacter]
+        await sut.loadCharactersAwait()
+    }
+
+    private func thenAssertLoadCharactersAwaitLoad() async {
+        try! await Task.sleep(for: .seconds(0.1))
+        XCTAssertTrue(getCharacterSpy.invokedExecute)
+        XCTAssertEqual(getCharacterSpy.invokedExecuteCount, 1)
+        XCTAssertEqual(self.sut.getQuantityOfCharacters, 2)
+        XCTAssertEqual(self.sut.characterList.first?.id, 1)
+        XCTAssertEqual(self.sut.characterList.first?.name, "Bronco")
+        XCTAssertEqual(self.sut.characterList.first?.description, "The best Marvel character")
+    }
+}
